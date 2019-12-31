b50a1188d225ca947dedc2147ebcfbd4bc9f5c32
NaCl10 <gabedschmitz@icloud.com>
2019-12-31 16:22:38 -0600

    Adding echos to state what the script is doing

--- install-motd-macos/install-motd-macos.sh
+++ install-motd-macos/install-motd-macos.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+echo "###############################################################################################################"
+echo "#Installing Homebrew. Make sure you don't have the Xcode command line tools installed and follow the prompsts.#"
+echo "###############################################################################################################"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "#############################################################"
+echo "#Installing Neofetch. Follow the prompts (if there are any).#"
+echo "#############################################################"
 brew install neofetch
 
+echo "#########################################################################"
+echo "#Editing /etc/profile/. Please enter your password. DO NOT press CTRL+C.#"
+echo "#########################################################################"
 sudo ./edit-profile.sh
